116402ef7459f5ea715eea1636bbefc912ed0e4c
Justin <xhergz@gmail.com>
2023-09-29 14:27:56 -0400

    I forgot the return statement because I am dumb.

--- src/app/api/interactions/route.ts
+++ src/app/api/interactions/route.ts
@@ -2,5 +2,5 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   console.log(JSON.stringify(req.body));
-  NextResponse.json({}, { status: 200 });
+  return NextResponse.json({}, { status: 200 });
 }
