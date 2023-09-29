c735ae1e2e7813f96b647b5f884c3096b156c7ca
Justin <xhergz@gmail.com>
2023-09-29 14:24:45 -0400

    Return 200.

--- src/app/api/interactions/route.ts
+++ src/app/api/interactions/route.ts
@@ -1,5 +1,6 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   console.log(JSON.stringify(req.body));
+  NextResponse.json({}, { status: 200 });
 }
