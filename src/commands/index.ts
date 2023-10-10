52ff34979e0554bd199ed72d4571515586d39462
Justin <xhergz@gmail.com>
2023-10-09 22:03:05 -0400

    Fix build issue.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,29 @@
 import { DiscordBotCommand } from "../types/command";
 import TestCommand from "./test";
-import MonsterCodesCommand from "./monster-codes";
+import { SlashCommandBuilder } from "discord.js";
 
-const ALL_COMMANDS: DiscordBotCommand[] = [MonsterCodesCommand, TestCommand];
+const monsterCodesCommand: DiscordBotCommand = {
+  data: new SlashCommandBuilder()
+    .setName("monster-codes")
+    .setDescription("Lists all the currently supported monster codes."),
+  execute: async (interaction) => {
+    await interaction.reply({
+      content: "Hello!",
+    });
+  },
+  /*execute: async (interaction) => {
+      const codes = Object.keys(MONSTER_SPAWN_DATA);
+      const listString = codes.map((code) => {
+        const monsterName = MONSTER_SPAWN_DATA[code].name;
+        return `${code} - ${monsterName}`;
+      });
+      //analytics.listedMonsterCodes(interaction.guildId!, interaction.user.id);
+      await interaction.reply({
+        content: codeBlock(listString.join("\n")),
+      });
+    },*/
+};
+
+const ALL_COMMANDS: DiscordBotCommand[] = [monsterCodesCommand, TestCommand];
 
 export { ALL_COMMANDS };
